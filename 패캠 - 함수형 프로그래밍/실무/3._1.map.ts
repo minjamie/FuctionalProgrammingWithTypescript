9829f1b120208f18c39ffe1941ce56f2bbd14a81
minjaekim <minjae2246@gmail.com>
2023-01-29 00:02:22 +0900

    map 함수

---
+++ 패캠 - 함수형 프로그래밍/실무/3._1.map.ts
@@ -0,0 +1,8 @@
+
+const map =<A, B> (array:Array<A>, f:(a:A)=>B): Array<B> => {
+  const result: Array<B> = [];
+  for (const value of array) {
+    result.push(f(value))
+  }  
+  return result;
+}
