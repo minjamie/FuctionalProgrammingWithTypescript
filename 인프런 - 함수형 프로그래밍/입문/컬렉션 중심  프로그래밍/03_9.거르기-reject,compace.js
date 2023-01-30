4f9b294b85498800a8f2b64d4fb277953cfa6282
minjaekim <minjae2246@gmail.com>
2023-01-30 23:56:06 +0900

    find 거르기 함수

--- 인프런 - 함수형 프로그래밍/입문/컬렉션 중심  프로그래밍/03_9.거르기-reject,compace.js
+++ 인프런 - 함수형 프로그래밍/입문/컬렉션 중심  프로그래밍/03_9.거르기-reject,compace.js
@@ -37,3 +37,33 @@ function _reject(data, predi){
 const _compact = _filter(_identity)
 
 _compact([1, 2, 0, false, null, {}])
+
+
+//some 
+function _some(data, predi){
+  predi = predi || _identity 
+  return _find_index(data, predi || _identity) != -1;
+}
+// 하나라도 만족하는 값 나오면 트루 반환
+
+_some([1,2,5,10,20], function(val){
+  return val > 10
+})
+
+// every - 하나라도 거짓이면 펄스이면 리턴
+function _every(data,predi){
+  return _find_index(data,_negate(predi || _identity)) == -1
+}
+
+very([1,2,5,10,20], function(val){
+  return val > 3
+})
+
+// true
+_every([1,2,0,10])
+// false
+_some([null, false, 0])
+
+_some(users, function(user){
+      return user.age>20
+      })
